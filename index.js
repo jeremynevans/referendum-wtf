18cb2e8286a1ccd4184efa0070ba829fad31a756
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-05-12 17:50:19 +0100

    Both tabs

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/issues2', function(request, response) {
   response.render('pages/issues2');
 });
 
+app.get('/issues3', function(request, response) {
+  response.render('pages/issues3');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
