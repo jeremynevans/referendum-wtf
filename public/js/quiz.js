ae894d4f262aac805bc9d70b33d9aeb7e40f16ca
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-06-15 18:12:04 +0100

    Quiz updated

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -4,23 +4,161 @@ var score = 0;
 
 var currentRound = -1;
 var rounds = [
+  // {
+  //   question: "Roughly how much do we pay in fees to the EU every week?",
+  //   options: [
+  //     "£350m",
+  //     "£160m",
+  //     "£100m"
+  //   ],
+  //   answer: 1
+  // },
   {
-    question: "Roughly how much do we pay in fees to the EU every week?",
+    question: "Who is this? <img src='/quiz/nf.png'>",
     options: [
-      "£350m",
-      "£160m",
-      "£100m"
+      "Justin Bieber",
+      "Nigel Farage",
+      "Mr T",
+      "Kermit the frog"
     ],
     answer: 1
   },
   {
-    question: "Second question",
+    question: "What day is the EU referendum?",
     options: [
-      "£1m",
-      "£2m",
-      "£3m"
+      "23rd June",
+      "30th June",
+      "22nd June",
+      "7th July"
     ],
     answer: 0
+  },
+  {
+    question: "Where's the European Parliament headquarters?",
+    options: [
+      "London",
+      "Berlin",
+      "Brussels",
+      "Strasbourg"
+    ],
+    answer: 2
+  },
+  {
+    question: "How many states are a member of European Union?",
+    options: [
+      "30",
+      "22",
+      "19",
+      "28"
+    ],
+    answer: 3
+  },
+  {
+    question: "Can you move across EU borders without a passport?",
+    options: [
+      "Yes all the time",
+      "Sometimes",
+      "No, never!"
+    ],
+    answer: 1
+  },
+  {
+    question: "Who is this? <img src='/quiz/am.png'>",
+    options: [
+      "Teresa May",
+      "Will.I.Am",
+      "Angela Merkel",
+      "Christine Lagarde"
+    ],
+    answer: 2
+  },
+  {
+    question: "When did the Euro become a national currency?",
+    options: [
+      "1999",
+      "1992",
+      "1972",
+      "1990"
+    ],
+    answer: 0
+  },
+  {
+    question: "Did the UK join the EU during the 1992 Treaty of Maastricht or was it already a member of the European Economic Community?",
+    options: [
+      "1992 Treaty of Maastricht",
+      "Already a member of the European Economic Community"
+    ],
+    answer: 0
+  },
+  {
+    question: "How many member states use the Euro as their national currency?",
+    options: [
+      "22",
+      "27",
+      "19",
+      "30"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is the annual total net migration to the UK?",
+    options: [
+      "150,000",
+      "1,000,000",
+      "550,000",
+      "300,000"
+    ],
+    answer: 3
+  },
+  {
+    question: "How many Members of the European Parliament does the UK elect?",
+    options: [
+      "50",
+      "73",
+      "28",
+      "64"
+    ],
+    answer: 1
+  },
+  {
+    question: "What’s the spending limit for each side of the campaign?",
+    options: [
+      "£1 billion",
+      "£5 million",
+      "£7 million",
+      "£2 million"
+    ],
+    answer: 2
+  },
+  {
+    question: "How much trade do we have with the EU?",
+    options: [
+      "45% of all UK exports go to the EU",
+      "60% of all UK exports go to the EU",
+      "25% of all UK exports go to the EU",
+      "30% of all UK exports go to the EU"
+    ],
+    answer: 0
+  },
+  {
+    question: "Who's the president of the Euopean Parliament?",
+    options: [
+      "David Cameron",
+      "Martin Schulz",
+      "Jean-Claude Juncker",
+      "Federica Mogherini"
+    ],
+    answer: 1
+  },
+  {
+    question: "Who is this? <img src='/quiz/jcj.png'>",
+    options: [
+      "David Cameron",
+      "Martin Schulz",
+      "Federica Mogherini",
+      "Jean-Claude Juncker"
+    ],
+    answer: 3
   }
 ];
 
