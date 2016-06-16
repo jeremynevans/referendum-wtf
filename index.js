663ea0b7d1a9255effd3285d04657f09655f9cf1
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-06-16 19:38:32 +0100

    Watermark2

--- index.js
+++ index.js
@@ -40,25 +40,35 @@ app.get('/cards/:author/:image/:text', function(request, response) {
     w: 500
   });
 
+  var blendWatermark = 'http://referendum.wtf/img/generate/watermark.png';
+
   var imageUrl = client.buildURL("/" + image, {
     mark: factMark,
     markalign: 'center,middle',
-    txt: 'Factogram created by ' + author,
+    txt: 'Created by ' + author,
     txtalign: 'center%2Cbottom',
     txtsize: 20,
-    txtclr: 'ffcc01',
-    txtpad: 30,
+    txtclr: 'fff',
+    txtpad: 90,
+    // txtlineclr: 'ffffff',
+    // txtline: 1,
     txtshad: 10,
     fit: 'crop',
-    // blend: 'watermark.png',
+    blend64: blendWatermark,
+    bw: 600,
+    ba: 'bottom, center',
+    balph: 100,
+    bm: 'normal',
     // exp: '-3',
     w: 600,
     h: 400
   });
 
+  // console.log(blendWatermark);
+
   var tweet = 'https://twitter.com/intent/tweet?text=%23AreEUReady%20for%20the%20Referendum%3F%20Thought%20not%2C%20so%20check%20out%20referendum.wtf%20...%21%20' + encodeURIComponent('http://referendum.wtf/cards/' + image + '/' + encodeURIComponent(text)) + '&source=webclient';
 
-  response.render('pages/card', { imageUrl: imageUrl, text: text, tweet: tweet });
+  response.render('pages/card', { imageUrl: imageUrl, text: text, tweet: tweet, blendWatermark: blendWatermark });
 
 // var textUrl = 'https://assets.imgix.net/~text64?w=500&txtclr=fff&txt64=' + textFixed + '&txtsize=24&txtlead=0&txtpad=15&bg=80002228&txtfont=Avenir-Heavy';
 // var imageUrl = 'https://referendum-wtf.imgix.net/' + encodeURIComponent(image) + '?txtsize=14&txtclr=ff0&txtalign=center%2Cbottom&txt64=' + encodeURIComponent(new Buffer(brand).toString('base64')) + '&txtfont64=SGVsdmV0aWNhTmV1ZS1NZWRpdW0&markalign=center%2Cmiddle&mark64=' + textUrl + '&fit=crop&exp=-3&w=600';
