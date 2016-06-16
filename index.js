cdf6c23149d7897886023769ad79f3ad7ba3984a
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-06-16 20:16:09 +0100

    Twitter title & desc

--- index.js
+++ index.js
@@ -22,11 +22,7 @@ app.get('/', function(request, response) {
 app.get('/cards/:image/:text', function(request, response) {
   var image = request.params.image.replace(/[^a-z0-9\.]/g, '');
   var text = request.params.text;
-  // var author = request.params.author;
-
-  // var authorMark = client.buildURL("/~text", {
-  //
-  // });
+  var author = request.params.author;
 
   var factMark = client.buildURL("/~text", {
     txt64: text,
@@ -66,7 +62,7 @@ app.get('/cards/:image/:text', function(request, response) {
 
   var tweet = 'https://twitter.com/intent/tweet?text=%23AreEUReady%20for%20the%20Referendum%3F%20Thought%20not%2C%20so%20check%20out%20referendum.wtf%20...%21%20' + encodeURIComponent('http://referendum.wtf/cards/' + image + '/' + encodeURIComponent(text)) + '&source=webclient';
 
-  response.render('pages/card', { imageUrl: imageUrl, text: text, tweet: tweet, blendWatermark: blendWatermark });
+  response.render('pages/card', { imageUrl: imageUrl, text: text, author: author, tweet: tweet });
 
 // var textUrl = 'https://assets.imgix.net/~text64?w=500&txtclr=fff&txt64=' + textFixed + '&txtsize=24&txtlead=0&txtpad=15&bg=80002228&txtfont=Avenir-Heavy';
 // var imageUrl = 'https://referendum-wtf.imgix.net/' + encodeURIComponent(image) + '?txtsize=14&txtclr=ff0&txtalign=center%2Cbottom&txt64=' + encodeURIComponent(new Buffer(brand).toString('base64')) + '&txtfont64=SGVsdmV0aWNhTmV1ZS1NZWRpdW0&markalign=center%2Cmiddle&mark64=' + textUrl + '&fit=crop&exp=-3&w=600';
